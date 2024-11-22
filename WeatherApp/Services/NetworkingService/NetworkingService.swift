eca6b912c5a423663fdf22e07b907f9dfc06a03e
Jason Lew-Rapai <jasonjrr@users.noreply.github.com>
2024-11-21 21:38:33 -0800

    Remove unused code
    Remove unused code

--- WeatherApp/Services/NetworkingService/NetworkingService.swift
+++ WeatherApp/Services/NetworkingService/NetworkingService.swift
@@ -23,8 +23,6 @@ public protocol NetworkingServiceProtocol: AnyObject {
     /// `AsyncProxy` adhering to `NetworkingServiceAsyncProxyProtocol`
     /// - Returns: A concrete implementation of `NetworkingServiceAsyncProxyProtocol`
     var async: Self.AsyncProxy { get }
-    
-    func setHeader(_ identifier: any NetworkingHeadersCollection.HeaderIdentifier, to value: String?)
 }
 
 /// Proxy definition for `Publisher`-based networking
@@ -40,48 +38,9 @@ public protocol NetworkingServicePublishersProxyProtocol {
     ///
     /// - Parameters:
     ///   - url: The `URL` from which to fetch data.
-    ///   - headers: The headers to be included in the request.
-    /// - Returns: A publisher that delivers a `NetworkingServiceResponse` or an `Error` upon completion.
-    func fetch<Model>(from url: URL, headers: [any NetworkingHeadersCollection.HeaderIdentifier]?) -> AnyPublisher<NetworkingServiceResponse<Model>, Error> where Model: Codable
-    
-    /// Fetches data from the specified `URL` with optional parameters and decodes it into the provided `Model` type.
-    ///
-    /// - Parameters:
-    ///   - url: The `URL` from which to fetch data.
     ///   - parameters: Optional parameters for the request.
     /// - Returns: A publisher that delivers a `NetworkingServiceResponse` or an `Error` upon completion.
     func fetch<Model>(from url: URL, parameters: [String: String]?) -> AnyPublisher<NetworkingServiceResponse<Model>, Error> where Model: Codable
-    
-    /// Fetches data from the specified `URL` with optional parameters and decodes it into the provided `Model` type.
-    ///
-    /// - Parameters:
-    ///   - url: The `URL` from which to fetch data.
-    ///   - headers: The headers to be included in the request.
-    ///   - parameters: Optional parameters for the request.
-    /// - Returns: A publisher that delivers a `NetworkingServiceResponse` or an `Error` upon completion.
-    func fetch<Model>(from url: URL, headers: [any NetworkingHeadersCollection.HeaderIdentifier]?, parameters: [String: String]?) -> AnyPublisher<NetworkingServiceResponse<Model>, Error> where Model: Codable
-    
-    /// Fetches an image from the specified URL using the provided `URLRequest`.
-    ///
-    /// - Parameters:
-    ///   - request: The `URLRequest` for fetching the image.
-    /// - Returns: A publisher that delivers an `NetworkingServiceImageResponse` or an `Error` upon completion.
-    func fetchImage(from request: URLRequest) -> AnyPublisher<NetworkingServiceImageResponse, Error>
-    
-    /// Fetches an image from the specified `URL`.
-    ///
-    /// - Parameters:
-    ///   - url: The `URL` from which to fetch the image.
-    /// - Returns: A publisher that delivers an `NetworkingServiceImageResponse` or an `Error` upon completion.
-    func fetchImage(from url: URL) -> AnyPublisher<NetworkingServiceImageResponse, Error>
-    
-    /// Sends a POST request with the input data to the specified `URL` and decodes the output into the provided `Model` type.
-    ///
-    /// - Parameters:
-    ///   - input: The input data to be sent in the request body.
-    ///   - url: The URL to which the POST request is sent.
-    /// - Returns: A publisher that delivers a response or an error upon completion.
-    func post<Input, Output>(_ input: Input, toURL url: URL) -> AnyPublisher<NetworkingServiceResponse<Output>, Error> where Input: Codable, Output: Codable
 }
 
 /// Proxy definition for async-based networking
@@ -91,24 +50,9 @@ public protocol NetworkingServiceAsyncProxyProtocol {
     ///
     /// - Parameters:
     ///   - url: The `URL` from which to fetch data.
-    ///   - headers: The headers to be included in the request.
     ///   - parameters: Optional parameters for the request.
     /// - Returns: A `NetworkingServiceResponse` containing the decoded `Model` data.
-    func fetch<Model>(from url: URL, headers: [any NetworkingHeadersCollection.HeaderIdentifier]?, parameters: [String: String]?) async throws -> NetworkingServiceResponse<Model> where Model: Codable
-    
-    /// Asynchronously fetches an image from the specified URL using the provided `URLRequest`.
-    ///
-    /// - Parameters:
-    ///   - request: The `URLRequest` for fetching the image.
-    /// - Returns: A `NetworkingServiceImageResponse` containing the fetched image.
-    func fetchImage(from request: URLRequest) async throws -> NetworkingServiceImageResponse
-    
-    /// Asynchronously fetches an image from the specified `URL`.
-    ///
-    /// - Parameters:
-    ///   - url: The `URL` from which to fetch the image.
-    /// - Returns: A `NetworkingServiceImageResponse` containing the fetched image.
-    func fetchImage(from url: URL) async throws -> NetworkingServiceImageResponse
+    func fetch<Model>(from url: URL, parameters: [String: String]?) async throws -> NetworkingServiceResponse<Model> where Model: Codable
 }
 
 /// `NetworkingService` implementation for centralizing, and configuring your requests and caching
@@ -134,7 +78,6 @@ public final class NetworkingService: NetworkingServiceProtocol {
     }
     
     public let configuration: URLSessionConfiguration
-    public let headersCollection: NetworkingHeadersCollection = NetworkingHeadersCollection()
     public var imageURLCache: URLCache?
     
     public init(configuration: URLSessionConfiguration = .default, imageURLCache: URLCache? = nil) {
@@ -144,7 +87,6 @@ public final class NetworkingService: NetworkingServiceProtocol {
     
     private func fetch<Model>(
         from url: URL,
-        headers: [any NetworkingHeadersCollection.HeaderIdentifier]? = nil,
         parameters: [String: String]? = nil
     ) -> AnyPublisher<NetworkingServiceResponse<Model>, Error> where Model: Codable {
         let finalURL: URL
@@ -163,10 +105,7 @@ public final class NetworkingService: NetworkingServiceProtocol {
             finalURL = url
         }
         
-        var urlRequest = URLRequest(url: finalURL)
-        headers?.forEach { header in
-            urlRequest.setValue(self.headersCollection[header], forHTTPHeaderField: header.key)
-        }
+        let urlRequest = URLRequest(url: finalURL)
         
         return URLSession(configuration: self.configuration)
             .dataTaskPublisher(for: urlRequest)
@@ -175,67 +114,6 @@ public final class NetworkingService: NetworkingServiceProtocol {
                 return NetworkingServiceResponse(model: model, response: response)
             }
             .eraseToAnyPublisher()
-    }
-    
-    private func fetchImage(from request: URLRequest) -> AnyPublisher<NetworkingServiceImageResponse, Error> {
-        if let response = self.imageURLCache?.cachedResponse(for: request),
-            let image = UIImage(data: response.data) {
-            return Just(NetworkingServiceImageResponse(image: image, data: response.data, response: response.response))
-                .setFailureType(to: Error.self)
-                .eraseToAnyPublisher()
-        } else {
-            return URLSession(configuration: self.configuration)
-                .dataTaskPublisher(for: request)
-                .tryMap { [imageURLCache] (data: Data, response: URLResponse) in
-                    guard let image = UIImage(data: data) else {
-                        throw NetworkingServiceError.failedToCreateImageFromData
-                    }
-                    imageURLCache?.storeCachedResponse(CachedURLResponse(response: response, data: data), for: request)
-                    return NetworkingServiceImageResponse(image: image, data: data, response: response)
-                }
-                .eraseToAnyPublisher()
-        }
-    }
-    
-    private func fetchImage(from url: URL) -> AnyPublisher<NetworkingServiceImageResponse, Error> {
-        let request = URLRequest(url: url)
-        return fetchImage(from: request)
-    }
-    
-    private func post<Input, Output>(
-        _ input: Input,
-        toURL url: URL,
-        headers: [any NetworkingHeadersCollection.HeaderIdentifier]? = nil
-    ) -> AnyPublisher<NetworkingServiceResponse<Output>, Error> where Input: Codable, Output: Codable {
-        let configuration = self.configuration
-        return Just(input)
-            .encode(encoder: Self.jsonEncoder)
-            .map { data in
-                var urlRequest = URLRequest(url: url)
-                urlRequest.httpMethod = "POST"
-                urlRequest.httpBody = data
-                urlRequest.setValue("application.json", forHTTPHeaderField: "Content-Type")
-                
-                headers?.forEach { header in
-                    urlRequest.setValue(self.headersCollection[header], forHTTPHeaderField: header.key)
-                }
-                
-                return urlRequest
-            }
-            .flatMap { urlRequest in
-                URLSession(configuration: configuration)
-                    .dataTaskPublisher(for: urlRequest)
-                    .tryMap { (data: Data, response: URLResponse) in
-                        let model = try Self.jsonDecoder.decode(Output.self, from: data)
-                        return NetworkingServiceResponse(model: model, response: response)
-                    }
-                    .eraseToAnyPublisher()
-            }
-            .eraseToAnyPublisher()
-    }
-    
-    public func setHeader(_ identifier: any NetworkingHeadersCollection.HeaderIdentifier, to value: String?) {
-        self.headersCollection[identifier] = value
     }
 }
 
@@ -252,28 +130,8 @@ extension NetworkingService {
             self.service.fetch(from: url)
         }
         
-        public func fetch<Model>(from url: URL, headers: [any NetworkingHeadersCollection.HeaderIdentifier]?) -> AnyPublisher<NetworkingServiceResponse<Model>, any Error> where Model : Decodable, Model : Encodable {
-            self.service.fetch(from: url, headers: headers)
-        }
-        
         public func fetch<Model>(from url: URL, parameters: [String: String]?) -> AnyPublisher<NetworkingServiceResponse<Model>, Error> where Model: Decodable, Model: Encodable {
             self.service.fetch(from: url, parameters: parameters)
-        }
-        
-        public func fetch<Model>(from url: URL, headers: [any NetworkingHeadersCollection.HeaderIdentifier]?, parameters: [String : String]?) -> AnyPublisher<NetworkingServiceResponse<Model>, any Error> where Model : Decodable, Model : Encodable {
-            self.service.fetch(from: url, headers: headers, parameters: parameters)
-        }
-        
-        public func fetchImage(from request: URLRequest) -> AnyPublisher<NetworkingServiceImageResponse, Error> {
-            self.service.fetchImage(from: request)
-        }
-        
-        public func fetchImage(from url: URL) -> AnyPublisher<NetworkingServiceImageResponse, Error> {
-            self.service.fetchImage(from: url)
-        }
-        
-        public func post<Input, Output>(_ input: Input, toURL url: URL) -> AnyPublisher<NetworkingServiceResponse<Output>, Error> where Input: Codable, Output: Codable {
-            self.service.post(input, toURL: url)
         }
     }
     
@@ -293,18 +151,9 @@ extension NetworkingService {
         
         public func fetch<Model>(
             from url: URL,
-            headers: [any NetworkingHeadersCollection.HeaderIdentifier]?,
             parameters: [String: String]?
         ) async throws -> NetworkingServiceResponse<Model> where Model: Decodable, Model: Encodable {
-            try await self.service.fetch(from: url, headers: headers, parameters: parameters).async()
-        }
-        
-        public func fetchImage(from request: URLRequest) async throws -> NetworkingServiceImageResponse {
-            try await self.service.fetchImage(from: request).async()
-        }
-        
-        public func fetchImage(from url: URL) async throws -> NetworkingServiceImageResponse {
-            try await self.service.fetchImage(from: url).async()
+            try await self.service.fetch(from: url, parameters: parameters).async()
         }
     }
     
--- WeatherAppTests/_MOCKS/MOCKNetworkingService.swift
+++ WeatherAppTests/_MOCKS/MOCKNetworkingService.swift
@@ -31,28 +31,6 @@ class MockNetworkingService<TestModel: Decodable>: NetworkingServiceProtocol {
     func fetch<Model>(from url: URL, parameters: [String : String]?) -> AnyPublisher<NetworkingServiceResponse<Model>, any Error> where Model : Decodable {
         fatalError("Not implemented")
     }
-    
-    var mock_fetchImageFromURLRequest: ((_ request: URLRequest) -> AnyPublisher<NetworkingServiceImageResponse, any Error>)?
-    func fetchImage(from request: URLRequest) -> AnyPublisher<NetworkingServiceImageResponse, any Error> {
-        if let mock_fetchImageFromURLRequest {
-            return mock_fetchImageFromURLRequest(request)
-        } else {
-            fatalError(Errors.mockClosureNotImplemented.localizedDescription)
-        }
-    }
-    
-    var mock_fetchImageFromURL: ((_ url: URL) -> AnyPublisher<NetworkingServiceImageResponse, any Error>)?
-    func fetchImage(from url: URL) -> AnyPublisher<NetworkingServiceImageResponse, any Error> {
-        if let mock_fetchImageFromURL {
-            return mock_fetchImageFromURL(url)
-        } else {
-            fatalError(Errors.mockClosureNotImplemented.localizedDescription)
-        }
-    }
-    
-    func setHeader(_ identifier: any NetworkingHeadersCollection.HeaderIdentifier, to value: String?) {
-        fatalError("Not implemented")
-    }
 }
 
 extension MockNetworkingService {
@@ -75,25 +53,9 @@ extension MockNetworkingService {
         public func fetch<Model>(from url: URL) -> AnyPublisher<NetworkingServiceResponse<Model>, Error> where Model: Decodable, Model: Encodable {
             self.service.fetch(from: url)
         }
-        
-        public func fetch<Model>(from url: URL, headers: [any NetworkingHeadersCollection.HeaderIdentifier]?) -> AnyPublisher<NetworkingServiceResponse<Model>, any Error> where Model : Decodable, Model : Encodable {
-            self.service.fetch(from: url)
-        }
-        
+    
         public func fetch<Model>(from url: URL, parameters: [String: String]?) -> AnyPublisher<NetworkingServiceResponse<Model>, Error> where Model: Decodable, Model: Encodable {
             self.service.fetch(from: url, parameters: parameters)
-        }
-        
-        public func fetch<Model>(from url: URL, headers: [any NetworkingHeadersCollection.HeaderIdentifier]?, parameters: [String : String]?) -> AnyPublisher<NetworkingServiceResponse<Model>, any Error> where Model : Decodable, Model : Encodable {
-            self.service.fetch(from: url, parameters: parameters)
-        }
-        
-        public func fetchImage(from request: URLRequest) -> AnyPublisher<NetworkingServiceImageResponse, Error> {
-            self.service.fetchImage(from: request)
-        }
-        
-        public func fetchImage(from url: URL) -> AnyPublisher<NetworkingServiceImageResponse, Error> {
-            self.service.fetchImage(from: url)
         }
         
         public func post<Input, Output>(_ input: Input, toURL url: URL) -> AnyPublisher<NetworkingServiceResponse<Output>, Error> where Input: Codable, Output: Codable {
@@ -117,18 +79,9 @@ extension MockNetworkingService {
         
         public func fetch<Model>(
             from url: URL,
-            headers: [any NetworkingHeadersCollection.HeaderIdentifier]?,
             parameters: [String: String]?
         ) async throws -> NetworkingServiceResponse<Model> where Model: Decodable, Model: Encodable {
             try await self.service.fetch(from: url).async()
-        }
-        
-        public func fetchImage(from request: URLRequest) async throws -> NetworkingServiceImageResponse {
-            try await self.service.fetchImage(from: request).async()
-        }
-        
-        public func fetchImage(from url: URL) async throws -> NetworkingServiceImageResponse {
-            try await self.service.fetchImage(from: url).async()
         }
     }
     
